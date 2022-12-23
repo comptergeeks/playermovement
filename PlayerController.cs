97674eebe9aafaaf2704766e6c79f0c7957b5c8e
comptergeeks <53153204+comptergeeks@users.noreply.github.com>
2022-12-23 01:39:06 -0800

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Camera playerCameraValue; 
 
     [SerializeField] Transform playerCamera = null;
-    [SerializeField] float mouseSensitivityX = 1f;
-    [SerializeField] float mouseSensitivityY = 1f;
+    [SerializeField][Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
+    [SerializeField] float mouseSensitivity = 3.5f;
     float xRotation; 
     float yRotation; 
     Vector2 currentDirVelocity = Vector2.zero; 
@@ -25,10 +26,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField][Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f; 
    
     [SerializeField] float gravity = -13.0f; 
+    [SerializeField] float springSpeed; 
+    Vector2 currentMouseDelta;
+    Vector2 currentMouseDeltaVelocity;
     Vector2 currentDir = Vector2.zero; 
     public bool isSprinting = false;
-
+    float cameraCap; 
     float velocityY = 0.0f; 
+    public float t = 0.5f;
+    public float maxFOV;
+    public float startFOV; 
     // Start is called before the first frame update
     void Start()
     {
@@ -51,14 +58,17 @@ public class PlayerController : MonoBehaviour
         
     }
     void UpdateMouseLook() {
-        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivityX;
-        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivityY;
-
-        yRotation += mouseX; 
-        xRotation -= mouseY; 
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); 
-
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+ 
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+ 
+        cameraCap -= currentMouseDelta.y * mouseSensitivity;
+ 
+        cameraCap = Mathf.Clamp(cameraCap, -90.0f, 90.0f);
+ 
+        playerCamera.localEulerAngles = Vector3.right * cameraCap;
+ 
+        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
          
     
 
@@ -73,18 +83,23 @@ public class PlayerController : MonoBehaviour
 
         if(Input.GetKey(KeyCode.LeftShift)) {
             isSprinting = true;
+            playerCameraValue.fieldOfView = Mathf.Lerp(playerCameraValue.fieldOfView, maxFOV, t);
         } else {
             isSprinting = false; 
+            playerCameraValue.fieldOfView = Mathf.Lerp(playerCameraValue.fieldOfView, startFOV, t);
         }
 
         Vector3 velocity = (transform.forward * targetDir.y + transform.right * targetDir.x) * playerSpeed + Vector3.up * velocityY;
         if(isSprinting) {
-            velocity *= 1.5f;  
+            velocity *= springSpeed;  
         }
         controller.Move(velocity * Time.deltaTime); 
         if(isGrounded! && controller.velocity.y < -1f)
         {
             velocityY = -8f;
+        }
+        if(Input.GetKey(KeyCode.Space) && isGrounded) {
+               velocityY += 6f;  
         }
     }
 }
